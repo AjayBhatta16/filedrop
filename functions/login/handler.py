ee6b5057e9a03982f3bb052bc4d62eb5d24b4eaf
AjayBhatta16 <ajay.bhattacharyya.16@gmail.com>
2025-04-14 00:46:50 -0400

    Improved Login Function Draft + CI/CD pipeline

--- functions/login/handler.py
+++ functions/login/handler.py
@@ -6,43 +6,62 @@ class LoginHandler():
         self.file_metadata_repo = file_metadata_repo
 
     def get_user(self, username: str):
+        LOG_CONTEXT = 'LoginHandler - get_user'
+
+        print(f'{LOG_CONTEXT} - search by username')
         username_query = self.user_repo.search({"username": username})
 
         if len(list(username_query)) > 0:
             return list(username_query)[0]
         
+        print(f'{LOG_CONTEXT} - search by email')
         email_query = self.user_repo.search({"email": username})
 
         if len(list(email_query)) > 0:
             return list(email_query)[0]
         
+        print(f'{LOG_CONTEXT} - user not found')
         raise HttpException(404, "User not found")
     
     def check_password(req_password: str, db_password: str):
-        if req_password == db_password or req_password == encrypt_password(db_password):
+        LOG_CONTEXT = 'LoginHandler - check_password'
+
+        print(f'{LOG_CONTEXT} - encrypting password')
+        encrypt_result = encrypt_password(db_password)
+
+        if req_password == db_password or req_password == encrypt_result:
             return True
         
+        print(f'{LOG_CONTEXT} - Incorrect password')
         raise HttpException(401, "Incorrect password")
     
-    def get_user_files(self, username):
-        file_query = self.file_metadata_repo.search({"ownerID": username})
+    def get_user_files(self, user):
+        LOG_CONTEXT = 'LoginHandler - get_user_files'
+
+        print(f'{LOG_CONTEXT} - searching for files for owner {user["username"]}')
 
         user_files = []
-        for file in file_query:
-            user_files.append({
-                "id": file['id'],
-                "name": file['name'],
-                "type": file['type'],
-                "expDate": file['expDate']
-            })
+
+        if "file_list" in user:
+            for file_id in user["file_list"]:
+                file = self.file_metadata_repo.search_one({"displayID": file_id})
+
+                user_files.append({
+                    "id": file['id'],
+                    "name": file['name'],
+                    "type": file['type'],
+                    "expDate": file['expDate']
+                })
+
+        print(f'{LOG_CONTEXT} - {len(user_files)} files found')
         
         return user_files
 
     def handle(self, req):
-        user = self.get_user(req.username)
+        user = self.get_user(req["username"])
 
-        self.check_password(req.password, user.password)
+        self.check_password(req["password"], user["password"])
 
-        user.files = self.get_user_files(user.username)
+        user.files = self.get_user_files(user)
 
         return user
--- functions/login/lambda_function.py
+++ functions/login/lambda_function.py
@@ -16,19 +16,43 @@ def lambda_handler(event, context):
 
         result = req_handler.handle(req_body)
 
-        return json.dumps({
-            "status": 200,
-            "data": result
-        })
+        print(f'Handler result -', result)
+
+        return {
+            "statusCode": 201,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "isBase64Encoded": False,
+            "body": json.dumps({
+                "username": result["username"],
+                "email": result["email"],
+                "files": result["files"]
+            }),
+        }
 
     except HttpException as ex:
-        return json.dumps({
-            "status": ex.status_code,
-            "message": ex.message
-        })
+        return {
+            "statusCode": ex.status_code,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "error": ex.message
+            }),
+            "isBase64Encoded": False
+        }
     
     except Exception as ex:
-        return json.dumps({
-            "status": 500,
-            "message": "An unknown error has occurred."
-        })
+        print(str(ex))
+
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "error": "An unknown error has occurred."
+            }),
+            "isBase64Encoded": False
+        }
