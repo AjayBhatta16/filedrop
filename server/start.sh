a48fafbde58de56bc36e9120149cfa7084b7b2c0
AjayBhatta16 <ajay.bhattacharyya.16@gmail.com>
2025-04-18 21:59:34 -0400

    better manual start script for Flask API

--- server/start.sh
+++ server/start.sh
@@ -1,1 +1,12 @@
-export API_GATEWAY_URL=$API_GATEWAY_URL && export S3_BUCKET_NAME=$S3_BUCKET_NAME && python3 application.py && echo
+#!/bin/bash
+CONFIG_FILE_PATH="../functions/env.json"
+
+eval $(python3 -c "
+import json
+with open('$CONFIG_FILE_PATH') as f:
+    config = json.load(f)
+for k, v in config.items():
+    print(f'export {k}={json.dumps(str(v))}')
+")
+
+python3 application.py && echo
