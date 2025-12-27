0313ed8c13244ebd37a29f43bf48b38534a3fed0
AjayBhatta16 <ajay.bhattacharyya.16@gmail.com>
2025-12-27 00:09:19 -0500

    bum ass CoPilot doesn't know how to spell

--- functions/create-file/lambda_function.py
+++ functions/create-file/lambda_function.py
@@ -18,7 +18,7 @@ def lambda_handler(event, context):
 
         req_body = validate_request(event, req_schema)
 
-        req_body["createdByIP"] = middleware.get_requester_ip(event)
+        req_body["createdByIP"] = middleware.get_ip_address(event)
 
         result = req_handler.handle(req_body)
 
